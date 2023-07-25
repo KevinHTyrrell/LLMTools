a5ab0f2a6e0106e9764da7faee3dca7696dd4029
KevinHTyrrell <KevinHTyrrell@gmail.com>
2023-07-25 19:53:11 -0400

    Added map for internal to external id

--- vectordb/vector_db.py
+++ vectordb/vector_db.py
@@ -8,13 +8,14 @@ class VectorDB:
             self,
             n_dims: int
     ):
-        self._n_dims            = n_dims,
-        self._index             = faiss.IndexIDMap(faiss.IndexFlatL2(n_dims))
-        self._curr_id           = 0
-        self._index_built       = False
-        self._id_id_map         = {}
-        self._id_vector_map     = {}
-        self._id_metadata_map   = {}
+        self._n_dims                    = n_dims,
+        self._index                     = faiss.IndexIDMap(faiss.IndexFlatL2(n_dims))
+        self._curr_id                   = 0
+        self._index_built               = False
+        self._external_internal_id_map  = {}
+        self._internal_external_id_map  = {}
+        self._id_vector_map             = {}
+        self._id_metadata_map           = {}
 
     def add_vectors(
             self,
@@ -34,7 +35,8 @@ class VectorDB:
             id_list.append(id_internal)
             self._id_vector_map[id_internal] = vector
             if ids is not None:
-                self._id_id_map[id_internal] = ids[i]
+                self._external_internal_id_map[ids[i]] = id_internal
+                self._internal_external_id_map[id_internal] = ids[i]
             if metadata is not None:
                 self._id_metadata_map[id_internal] = metadata[i]
             self._curr_id += 1
@@ -43,7 +45,7 @@ class VectorDB:
 
     def get_vector(self, id):
         assert self._index_built, 'INDEX MUST CONTAIN VECTORS BEFORE ACCESS'
-        internal_id = self._id_id_map[id]
+        internal_id = self._external_internal_id_map[id]
         return self._id_vector_map[internal_id]
 
     def get_neighbors(
@@ -55,11 +57,12 @@ class VectorDB:
     ):
         assert (vector is not None) or (id is not None), 'MUST PROVIDE VECTOR OR VECTOR_ID'
         if vector is None:
-            internal_id = self._id_id_map[id]
+            internal_id = self._external_internal_id_map[id]
             vector = self._id_vector_map[internal_id]
         vector = np.expand_dims(vector, axis=0) if len(vector.shape) == 1 else vector
         distances, internal_vector_ids = self._index.search(vector, k=k)
         distances, internal_vector_ids = distances.flatten(), internal_vector_ids.flatten()
+        vector_ids = [self._internal_external_id_map[id] for id in internal_vector_ids]
         matching_vectors = [self._id_vector_map[id] for id in internal_vector_ids]
         if return_metadata:
             assert len(self._id_metadata_map) > 0, 'NO METADATA PROVIDED'
@@ -67,6 +70,7 @@ class VectorDB:
         vector_info_list = []
         for i in range(len(matching_vectors)):
             vector_info = {
+                'id': vector_ids[i],
                 'distance': distances[i],
                 'vector': matching_vectors[i],
             }
@@ -77,5 +81,5 @@ class VectorDB:
 
     def add_metadata(self, ids, metadata: List[str]):
         for i in range(len(ids)):
-            internal_id = self._id_id_map[ids[i]]
+            internal_id = self._external_internal_id_map[ids[i]]
             self._id_metadata_map[internal_id] = metadata[i]
